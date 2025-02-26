4054a9ede7dcedb39d2b838afc3ceb7c4a6f7df4
Luiz Fernando da Silva <devpro.luiz@gmail.com>
2025-02-25 22:19:06 -0300

    Alterações diversas feitas

--- src/aula_gitgithub/Program.java
+++ src/aula_gitgithub/Program.java
@@ -3,8 +3,7 @@ package aula_gitgithub;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello, word!");
 	}
 
 }
---
+++ src/entities/Product.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class Product {
+	private Integer id;
+	private Integer quantity;
+	private String name;
+}
