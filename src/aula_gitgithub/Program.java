95eaed816502b148cf9f3539867a20bbd9d0242c
Luiz Fernando da Silva <devpro.luiz@gmail.com>
2025-02-25 22:45:36 -0300

    Atualizações no programa

--- src/aula_gitgithub/Program.java
+++ src/aula_gitgithub/Program.java
@@ -4,6 +4,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, word!");
+		System.out.print("Digite o id do usuário: ");
+		System.out.print("Digite o nome do usuário: ");
+		System.out.print("Digite a idade do usuário: ");
+		System.out.print("Digite a altura do usuário: ");
 	}
 
 }
